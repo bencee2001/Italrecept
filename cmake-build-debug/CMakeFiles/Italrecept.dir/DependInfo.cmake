eb933e4eb4f0764ddee74c2c00483cd42f477572
bencee2001 <74102469+bencee2001@users.noreply.github.com>
2022-05-11 16:10:12 +0200

    Kész

---
+++ cmake-build-debug/CMakeFiles/Italrecept.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Bence/Desktop/Italrecept/Alcohol.cpp" "C:/Users/Bence/Desktop/Italrecept/cmake-build-debug/CMakeFiles/Italrecept.dir/Alcohol.cpp.obj"
+  "C:/Users/Bence/Desktop/Italrecept/App.cpp" "C:/Users/Bence/Desktop/Italrecept/cmake-build-debug/CMakeFiles/Italrecept.dir/App.cpp.obj"
+  "C:/Users/Bence/Desktop/Italrecept/Bartender.cpp" "C:/Users/Bence/Desktop/Italrecept/cmake-build-debug/CMakeFiles/Italrecept.dir/Bartender.cpp.obj"
+  "C:/Users/Bence/Desktop/Italrecept/Fruit.cpp" "C:/Users/Bence/Desktop/Italrecept/cmake-build-debug/CMakeFiles/Italrecept.dir/Fruit.cpp.obj"
+  "C:/Users/Bence/Desktop/Italrecept/Ingredients.cpp" "C:/Users/Bence/Desktop/Italrecept/cmake-build-debug/CMakeFiles/Italrecept.dir/Ingredients.cpp.obj"
+  "C:/Users/Bence/Desktop/Italrecept/Logger.cpp" "C:/Users/Bence/Desktop/Italrecept/cmake-build-debug/CMakeFiles/Italrecept.dir/Logger.cpp.obj"
+  "C:/Users/Bence/Desktop/Italrecept/Recipe.cpp" "C:/Users/Bence/Desktop/Italrecept/cmake-build-debug/CMakeFiles/Italrecept.dir/Recipe.cpp.obj"
+  "C:/Users/Bence/Desktop/Italrecept/main.cpp" "C:/Users/Bence/Desktop/Italrecept/cmake-build-debug/CMakeFiles/Italrecept.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Italrecept.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Italrecept.dir/Alcohol.cpp.obj"
+  "CMakeFiles/Italrecept.dir/App.cpp.obj"
+  "CMakeFiles/Italrecept.dir/Bartender.cpp.obj"
+  "CMakeFiles/Italrecept.dir/Fruit.cpp.obj"
+  "CMakeFiles/Italrecept.dir/Ingredients.cpp.obj"
+  "CMakeFiles/Italrecept.dir/Logger.cpp.obj"
+  "CMakeFiles/Italrecept.dir/Recipe.cpp.obj"
+  "CMakeFiles/Italrecept.dir/main.cpp.obj"
+  "Italrecept.exe"
+  "Italrecept.exe.manifest"
+  "Italrecept.pdb"
+  "libItalrecept.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Italrecept.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
